757428be24b8cce9c39eda0b29c85a3535f07735
gregory fulgueiras <fulgueiras.gregory@gmail.com>
2024-10-14 22:53:25 +0200

    insert oeuvre to bdd

--- Premier_pas_PHP/Fulgueiras_Grégory_1_code102024/The-Artbox-master/header.php
+++ Premier_pas_PHP/Fulgueiras_Grégory_1_code102024/The-Artbox-master/header.php
@@ -13,7 +13,8 @@
         <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
         <nav>
             <ul>
-                <li><a href="index.html">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="ajouter.php">Ajouter une œuvre</a></li>
             </ul>
         </nav>
     </header>
--- Premier_pas_PHP/Fulgueiras_Grégory_1_code102024/The-Artbox-master/traitement.php
+++ Premier_pas_PHP/Fulgueiras_Grégory_1_code102024/The-Artbox-master/traitement.php
@@ -1,11 +1,26 @@
 <?php
 
-if (empty($_POST['titre']) || empty($_POST['artiste']) || empty($_POST['image']) 
+if (empty($_POST['titre']) 
+    || empty($_POST['artiste']) 
+    || empty($_POST['description'])
+    || empty($_POST['image']) 
     || strlen($_POST['description']) < 3 
     || !filter_var($_POST['image'], FILTER_VALIDATE_URL)
 ) {
     header('Location: ajouter.php');
 } else {
-    // insérer en bdd
+    $titre = htmlspecialchars($_POST['titre']);
+    $description = htmlspecialchars($_POST['description']);
+    $artiste = htmlspecialchars($_POST['artiste']);
+    $image = htmlspecialchars($_POST['image']);
+    
+    include('bdd.php');
+    $bdd = connexion();
+
+    $requête = $bdd->prepare('INSERT INTO oeuvres (titre, artiste, description, image) VALUES (?, ?, ?, ?)');
+    $requête->execute([$titre, $artiste, $description, $image]);
+
+    header('Location: oeuvre.php?id=' . $bdd->lastInsertId());
+
 }
 
